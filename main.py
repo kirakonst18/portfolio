9e854d2fb50a838e9b0ad65fedf595a95b264c23
kirakonst18 <konst181109@mail.ru>
2025-10-18 18:37:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ def process_form():
     button_python = request.form.get('button_python')
     button_tg = request.form.get('button_tg')
     button_js = request.form.get('button_js')
-    return render_template('index.html', button_python=button_python, button_tg=button_tg, button_js=button_js)
+    button_html = request.form.get('button_html')
+    button_db = request.form.get('button_db')
+    return render_template('index.html', button_python=button_python, button_tg=button_tg, button_js=button_js, button_html=button_html, button_db=button_db)
 
 
 if __name__ == "__main__":
